7320347e330061b063cb3a978c5b98046208e90b
dnovs0416 <davenovales@gmail.com>
2022-05-31 21:35:30 -0400

    1st commit

---
+++ src/gitReview/gitreview01.java
@@ -0,0 +1,12 @@
+package gitReview;
+
+public class gitreview01 {
+	
+	public static void main(String[] args) {
+		
+		System.out.print("today we learn github");
+		System.out.print("today is a very good dAY");
+		
+	}
+
+}
